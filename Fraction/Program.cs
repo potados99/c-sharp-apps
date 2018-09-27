05cc0a1a18e1cbe6b9836c6148c2f5337984b8f9
potados <potados99@gmail.com>
2018-09-27 14:56:02 +0900

    Modify usage

--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -6,13 +6,15 @@ namespace Fraction
     {
         static void Main(string[] args)
         {
-            var a = new Fraction(1,2);
-            var b = new Fraction(2,3);
+            var a = new Fraction(2,3);
+            var b = new Fraction(3,2);
+            var one = new Fraction(1);
 
             var c = a - b;
             var d = a + 2;
 
-            Console.WriteLine(c.ToString() + " " + d.ToString());
+            Console.WriteLine(a.ToString() + " - " + b.ToString() + " = " + c.ToString());
+            Console.WriteLine(a.ToString() + " + " + 2.ToString() + " = " + d.ToString());
 
             Console.Read();
         }
