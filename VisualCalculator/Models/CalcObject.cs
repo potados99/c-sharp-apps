64f75736990044a0989af51c514cdf526a2b3baa
potados99 <potados99@gmail.com>
2018-12-05 15:09:44 +0900

    Divide models into files

---
+++ VisualCalculator/Models/CalcObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualCalculator.Models
+{
+    public class CalcObject
+    {
+        public enum Type
+        {
+            NUM = 0x00,
+            OP_PLUS = 0x01,
+            OP_MINUS = 0x02,
+            OP_MULTIPLY = 0x01 << 0x04 | 0x01,
+            OP_DIVIDE = 0x01 << 0x04 | 0x02
+        }
+
+        public Type ObjectType;
+        public Double Number;
+
+        public CalcObject(Type type, double number)
+        {
+            this.ObjectType = type;
+            this.Number = number;
+        }
+
+        public override string ToString()
+        {
+            string returnStr = "";
+
+            switch (ObjectType)
+            {
+                case Type.NUM:
+                    returnStr = Number.ToString();
+                    break;
+                case Type.OP_PLUS:
+                    returnStr = "+";
+                    break;
+                case Type.OP_MINUS:
+                    returnStr = "-";
+                    break;
+                case Type.OP_MULTIPLY:
+                    returnStr = "*";
+                    break;
+                case Type.OP_DIVIDE:
+                    returnStr = "/";
+                    break;
+            }
+
+            return returnStr;
+        }
+    }
+}
